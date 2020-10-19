d8a0959bd25ca453cb31f4f2e2aceb78a40b021b
Kerwin Joel <joelss4029@gmail.com>
2020-10-19 18:15:48 -0500

    docs agregado

---
+++ docs/precache-manifest.c19099ed22fefff2fb458fc37f6efa54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887bbc5a44e9d5c49a7847175d99d923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1db3d5927aebd624397",
+    "url": "/static/css/main.1f65ab1e.chunk.css"
+  },
+  {
+    "revision": "580a77c3540062b34f99",
+    "url": "/static/js/2.53780636.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.53780636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1db3d5927aebd624397",
+    "url": "/static/js/main.532dfef3.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
